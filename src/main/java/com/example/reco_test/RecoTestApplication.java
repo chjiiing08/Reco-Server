425ed279dd3982eed4d4863902130691dd78f222
chjiiing08 <hyjinix@gmail.com>
2026-06-08 11:44:57 +0900

    fix: 메인 애플리케이션 클래스 추가 및 배포 오류 수정

---
+++ src/main/java/com/example/reco_test/RecoTestApplication.java
@@ -0,0 +1,11 @@
+package com.example.reco_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecoTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RecoTestApplication.class, args);
+    }
+}
